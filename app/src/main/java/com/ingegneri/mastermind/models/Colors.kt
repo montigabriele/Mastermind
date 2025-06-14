8880e35138be1b13d035e7fe7dd376491ef1038b
montigabriele <gabrielemonti.id@gmail.com>
2025-06-14 18:36:26 +0200

    Colors, History, Settings

--- app/src/main/java/com/ingegneri/mastermind/models/Colors.kt
+++ app/src/main/java/com/ingegneri/mastermind/models/Colors.kt
@@ -1,14 +1,20 @@
 package com.ingegneri.mastermind.models
+
 import androidx.compose.ui.graphics.Color
-enum class Colors(val color: Color){
-    RED(Color.Red),
-    GREEN(Color.Green),
-    BLUE(Color.Blue),
-    YELLOW(Color.Yellow),
-    BLACK(Color.Black),
-    WHITE(Color.White),
-    ORANGE(Color(0xFFFFA500)),
-    PINK(Color(0xFFFFC0CB)),
-    PURPLE(Color(0xFF800080)),
-    GRAY(Color.Gray)
+
+data class GameColors(
+    val red: Color = Color.Red,
+    val blue: Color = Color.Blue,
+    val green: Color = Color.Green,
+    val yellow: Color = Color.Yellow,
+    val purple: Color = Color(0xFF800080),
+    val orange: Color = Color(0xFFFFA500),
+    val cyan: Color = Color.Cyan,
+    val brown: Color = Color(0xFF8D6E63),
+    val pink: Color = Color(0xFFFFC0CB),
+    val lime: Color = Color(0xFF8BC34A),
+    val empty: Color = Color.Gray,
+    val black: Color = Color.Black
+) {
+    fun getColors(count: Int) = listOf(red, blue, green, yellow, purple, orange, cyan, brown, pink, lime).take(count)
 }
